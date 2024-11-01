dfe844006def3047f0147757a64e0c8bfcde91d1
Kenny Vigar <kennyvigar@Kennys-MacBook-Air.local>
2024-10-31 22:13:32 -0600

    (feat):working on mobile css

---
+++ src/Oct31.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function Oct31() {
+    return (
+        <>
+        <div className='blog-post'>
+            <div className='datestyle h3 '>Oct31st</div>
+                <p>So, the point of this post is to test the overflow CSS property in the blog components here...</p>
+                <p>.... cause I think it's broken</p>
+                <br></br>
+                <p></p>
+                <p>It's Halloween BTW!! Happy Halloween!!</p>     
+        </div>
+        </>
+    );
+}
+
+export default Oct31;
--- src/Posts.js
+++ src/Posts.js
@@ -1,19 +1,20 @@
 import './App.css';
 import Oct25 from './Oct25'
 import Oct27 from './Oct27'
+import Oct31 from './Oct31'
 
 
 function Posts() {
   return (
     <>
-    <div className="">
+    <div>
       
       <h4>Posts</h4>
-      <Oct27 />
-      <Oct25 />
-
-
-
+      <div className="posts">
+        <Oct31 />
+        <Oct27 />
+        <Oct25 />
+      </div>
     </div>
     </>
   );
